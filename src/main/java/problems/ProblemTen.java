98136f75389b38922deb9319f8c27b7e1e887435
zhsln <153474492+zhsln@users.noreply.github.com>
2024-04-06 23:46:22 +0500

    Added solution to problem #10.

---
+++ src/main/java/problems/ProblemTen.java
@@ -0,0 +1,31 @@
+package problems;
+
+import java.util.Scanner;
+
+public class ProblemTen {
+    public static void tenthProblem() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a: ");
+        int a = scanner.nextInt();
+        System.out.print("Enter b: ");
+        int b = scanner.nextInt();
+
+        System.out.print("GCD(" + a + "," + b + ") = " + calculateGCD(a, b) + ".");
+    }
+
+    /**
+     * This method calculates GCD(a,b) (greatest common divisor of a and b), where a and b are input integers.
+     * Time complexity: O(log(c)), where c is the min(a,b), because if a > b, GCD(b,a) = GCD(a,b), and vice-versa.
+     * The recursive algorithm makes recursive calls, decreasing the values of a and b until b is 0
+     *
+     * @param a the input integer.
+     * @param b the input integer.
+     * @return GCD(a,b) (greatest common divisor of a and b)
+     */
+    private static int calculateGCD(int a, int b) {
+        if (b == 0)
+            return a;
+
+        return calculateGCD(b, a % b);
+    }
+}
