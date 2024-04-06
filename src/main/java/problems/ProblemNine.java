d6d5cc04ea29bbc02724aff8330949ce50ae1f86
zhsln <153474492+zhsln@users.noreply.github.com>
2024-04-06 23:46:13 +0500

    Added solution to problem #9.

---
+++ src/main/java/problems/ProblemNine.java
@@ -0,0 +1,31 @@
+package problems;
+
+import java.util.Scanner;
+
+public class ProblemNine {
+    public static void ninthProblem() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = scanner.nextInt();
+        System.out.print("Enter k: ");
+        int k = scanner.nextInt();
+
+        System.out.println("Binomial coefficient, C(" + n + "," + k + "): " + calculateBinomialCoefficient(n, k) + ".");
+    }
+
+    /**
+     * This method calculates binomial coefficient C(n,k), where n and k are input integers.
+     * Time complexity: O(2^n), where n is the value of n.
+     * The recursive algorithm calls itself twice with modified parameters.
+     *
+     * @param n the input integer.
+     * @param k the input integer.
+     * @return integer C(n,k), where n and k are input integers.
+     */
+    private static int calculateBinomialCoefficient(int n, int k) {
+        if (k == 0 || k == n) // Base case.
+            return 1; // according to the task.
+
+        return calculateBinomialCoefficient(n - 1, k - 1) + calculateBinomialCoefficient(n - 1, k);
+    }
+}
